9cc44a1065f1ed8f6f293b4829fc50dc40a5c244
renebohne <rene.bohne@gmail.com>
2025-11-09 22:49:09 +0100

    simpler solution that only depends on Control-Surface library. Now with USBMIDI support for esp32s3

--- BLE/src/main.cpp
+++ BLE/src/main.cpp
@@ -1,57 +1,149 @@
+/**
+ * @file
+ * @brief This sketch turns an ESP32 into an advanced MIDI utility.
+ */
 #include <Arduino.h>
-#include "soc/soc_caps.h"
-#include <SAM2695_MIDI_Mapper.h>
+
+#include <Control_Surface.h>
 #include <MIDI_Interfaces/BluetoothMIDI_Interface.hpp>
-#include <MIDI_Interfaces/SerialMIDI_Interface.hpp>
+#include <MIDI_Interfaces/USBMIDI_Interface.hpp>
 
-#define DEVICE_NAME "XIAO-SYNTH"
+using namespace cs;
 
-#ifndef SOC_BLE_SUPPORTED	
-#error This ESP32 SoC has no BLE 
-#else
 
-#if defined(CONFIG_IDF_TARGET_ESP32S3)
-    #define BUTTON_A_PIN 4
-    #define BUTTON_B_PIN 3
-    #define BUTTON_C_PIN 2 
-    #define BUTTON_D_PIN 1 
-#elif defined(CONFIG_IDF_TARGET_ESP32C3)
-    #define BUTTON_A_PIN 5
-    #define BUTTON_B_PIN 4
-    #define BUTTON_C_PIN 3 
-    #define BUTTON_D_PIN 2 
-#elif defined(CONFIG_IDF_TARGET_ESP32C6)
-    #define BUTTON_A_PIN 21
-    #define BUTTON_B_PIN 2
-    #define BUTTON_C_PIN 1 
-    #define BUTTON_D_PIN 0
-#endif
+// --- Pin Definitions ---
+#define BUTTON_A_PIN 5 // Decrease Program
+#define BUTTON_B_PIN 4 // Increase Program
+#define BUTTON_C_PIN 3 // Toggle Bank
+#define BUTTON_D_PIN 2 // Toggle Reverb
 
 #if defined(CONFIG_IDF_TARGET_ESP32C3)
-    #define LED_PIN 10              
+    #define LED_PIN 10
+    #define LED_BUILTIN 10
 #else
     #define LED_PIN LED_BUILTIN
 #endif
 
-BluetoothMIDI_Interface bleMIDI;
-SerialMIDI_Interface sam_synth = {Serial0, 31250}; 
 
-SAM2695_MIDI_Mapper mapper(sam_synth);
+BluetoothMIDI_Interface midi_ble;
+HardwareSerialMIDI_Interface midi_ser {Serial0};
+BidirectionalMIDI_Pipe ble_pipe;
+
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+USBMIDI_Interface midi_usb;
+BidirectionalMIDI_Pipe usb_pipe;
+#endif
+
+AH::Button buttonA_phys = {BUTTON_A_PIN}; // Decrement
+AH::Button buttonB_phys = {BUTTON_B_PIN}; // Increment
+AH::Button buttonC_phys = {BUTTON_C_PIN}; // Toggle Bank
+AH::Button buttonD_phys = {BUTTON_D_PIN}; // Toggle Reverb
+
+// --- State Variables for Toggles ---
+bool mt32_mode = false;     // false = GM Mode (Bank 0), true = MT-32 Mode (Bank 127)
+bool reverbState = false; // false = Reverb Off, true = Reverb On
+uint8_t currentProgram[16] = {0}; // Holds the current program number (0-127) for each channel
+
+void toggleBank(cs::Channel chan) {
+  mt32_mode = !mt32_mode;
+  // CC #0 (Bank Select MSB)
+  // Value 0 = General MIDI Bank
+  // Value 127 = MT-32 Compatible Bank
+  uint8_t bankValue = mt32_mode ? 127 : 0;
+  Control_Surface.sendControlChange({0, chan}, bankValue);
+  // Resend the program for this channel to load the patch from the new bank
+  Control_Surface.sendProgramChange(chan, currentProgram[chan.getRaw()]);
+}
+
+void toggleReverb() {
+  reverbState = !reverbState;
+  // CC #91 is standard GM Reverb Send Level
+  // This function remains hard-coded to Channel 1
+  uint8_t reverbValue = reverbState ? 96 : 0;
+  Control_Surface.sendControlChange({91, Channel_1}, reverbValue);
+}
+
+void incrementProgram(cs::Channel chan) {
+  if (currentProgram[chan.getRaw()] < 127) {
+    currentProgram[chan.getRaw()]++;
+    Control_Surface.sendProgramChange(chan, currentProgram[chan.getRaw()]);
+  }
+}
+
+void decrementProgram(cs::Channel chan) {
+  if (currentProgram[chan.getRaw()] > 0) {
+    currentProgram[chan.getRaw()]--;
+    Control_Surface.sendProgramChange(chan, currentProgram[chan.getRaw()]);
+  }
+}
+
+// --- MIDI Input Callback Class ---
+class MyCallbacks : public MIDI_Callbacks {
+  public:
+    void onChannelMessage(MIDI_Interface &, ChannelMessage msg) override {
+      if (msg.getMessageType() == MIDIMessageType::ControlChange)
+      {
+        uint8_t controller = msg.getData1();
+        uint8_t value = msg.getData2();
+        if ( value > 0) {
+            if (controller == 14) {
+                decrementProgram(msg.getChannel());
+            } else if (controller == 15) {
+                incrementProgram(msg.getChannel());
+            } else if (controller == 13) {
+                toggleBank(msg.getChannel());
+            }
+        }
+      }
+      else if (msg.getMessageType() == MIDIMessageType::ProgramChange)
+      {
+        currentProgram[msg.getChannel().getRaw()] = msg.getData1();
+      }
+    }
+};
+
+MyCallbacks callbacks;
 
 void setup() {
-  sam_synth.begin(); 
+  midi_ble.setCallbacks(callbacks);
+
+  #if defined(CONFIG_IDF_TARGET_ESP32S3)
+  midi_usb.setCallbacks(callbacks);
+  #endif
+
+  midi_ble.setName("XIAO Synth");
   
-  bleMIDI.setName(DEVICE_NAME);
-  bleMIDI.begin();
+  midi_ser | ble_pipe | midi_ble;
+  
+  #if defined(CONFIG_IDF_TARGET_ESP32S3)
+  midi_ser | usb_pipe | midi_usb;
+  #endif
+  
+  buttonA_phys.begin();
+  buttonB_phys.begin();
+  buttonC_phys.begin();
+  buttonD_phys.begin();
 
-  mapper.begin();
-  bleMIDI.setCallbacks(mapper);
-
+  Control_Surface.begin();
+  
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  bleMIDI.update();
-  sam_synth.update();
-}
+  Control_Surface.loop();
 
-#endif 
+  if (buttonA_phys.update() == AH::Button::Falling) {
+    decrementProgram(Channel_1);
+  }
+  if (buttonB_phys.update() == AH::Button::Falling) {
+    incrementProgram(Channel_1);
+  }
+  if (buttonC_phys.update() == AH::Button::Falling) {
+    toggleBank(Channel_1);
+  }
+  if (buttonD_phys.update() == AH::Button::Falling) {
+    toggleReverb();
+  }
+  
+  digitalWrite(LED_BUILTIN, midi_ble.isConnected() ? HIGH : LOW);
+}
